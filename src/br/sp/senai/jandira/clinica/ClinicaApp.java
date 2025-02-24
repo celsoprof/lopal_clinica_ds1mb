29dc843f63f25763f643c3058b8e5d6e04348e82
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-02-24 11:21:36 -0300

    Métodos de acesso get e set concluídos

--- src/br/sp/senai/jandira/clinica/ClinicaApp.java
+++ src/br/sp/senai/jandira/clinica/ClinicaApp.java
@@ -1,5 +1,7 @@
 package br.sp.senai.jandira.clinica;
 
+import java.time.LocalDate;
+
 import br.sp.senai.jandira.clinica.model.Paciente;
 
 public class ClinicaApp {
@@ -12,17 +14,28 @@ public class ClinicaApp {
 		Paciente p2 = new Paciente();
 		System.out.println(p2);
 		
-		p1.setNome("");
+		p1.setNome("Ana Maria");
 		p1.setPeso(68);
 		p1.setAltura(1.66);
+		p1.setGenero("f");
+		p1.setTelefone("(11)98888-0002");
+		p1.setDataNascimento(LocalDate.of(2031, 2, 28));
 		
-		p2.setNome("Jó");
+		p2.setNome("Jó da Silva");
 		p2.setPeso(83);
 		p2.setAltura(1.67);
+		p2.setGenero("w");
+		p2.setTelefone("95555-0000");
 		
 		p1.exibirDados();
 		p2.exibirDados();
-
+		
+		System.out.println(p1.getNome().length());
+		System.out.println(p1.getNome().toUpperCase());
+		System.out.println(p1.getGenero());
+		System.out.println(p2.getGenero());
+		System.out.println();
+		
 	}
 
 }
--- src/br/sp/senai/jandira/clinica/model/Paciente.java
+++ src/br/sp/senai/jandira/clinica/model/Paciente.java
@@ -3,79 +3,111 @@ package br.sp.senai.jandira.clinica.model;
 import java.time.LocalDate;
 
 public class Paciente {
-	
+
 	private String nome;
-    private double peso;
-    private double altura;
-    private String genero;
-    private String telefone;
-    private LocalDate dataNascimento;
-    
-    public void setNome(String nome) {
-    	if (nome.length() > 3) {
-    		this.nome = nome;
-    	} else { 
-    		System.out.println("O nome deve conter mais do que 3 caracteres");
-    	}
-    }
-    
-    public void setAltura(double altura) {
-    	if(altura > 0.5) {
-    		this.altura = altura;
-    	} else {
-    		System.out.println("A altura deve ser maior do que 0,5m");
-    	}
-    }
-    
-    public double getAltura() {
-    	return altura;
-    }
-    
-    public void setPeso(double peso) {
-    	
-    	if(peso > 0) {
-    		// verdade
-    		this.peso = peso;
-    	} else {
-    		// falso
-    		System.out.println("O peso deve ser maior do que ZERO!");
-    	}
-    	
-    }
-    
-    public double getPeso() {
-    	return this.peso;
-    }
+	private double peso;
+	private double altura;
+	private String genero;
+	private String telefone;
+	private LocalDate dataNascimento;
+	
+	public void setTelefone(String telefone){
+		if (telefone.length() >= 13) {
+			this.telefone = telefone;
+		} else {
+			System.out.println("O valor " + telefone + " não é um telefone!");
+		}
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
-    public void marcarConsulta(){
+	public void setGenero(String genero) {
+		if (genero.equalsIgnoreCase("m") || genero.equalsIgnoreCase("f") || genero.equalsIgnoreCase("o")) {
+			this.genero = genero.toUpperCase();
+		} else {
+			System.out.println("O gênero do(a) " + nome + " deve ser \"M\" ou \"F\" ou \"O\"");
+		}
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
 
-    }
+	public void setNome(String nome) { // Inicio
+		if (nome.length() > 3) {
+			this.nome = nome.toUpperCase();
+		} else {
+			System.out.println("O nome deve conter mais do que 3 caracteres");
+		}
+	} // fim
 
-    public void calcularIdade() {
+	public String getNome() {
+		return nome;
+	}
 
-    }
+	public void setAltura(double altura) {
+		if (altura > 0.5) {
+			this.altura = altura;
+		} else {
+			System.out.println("A altura deve ser maior do que 0,5m");
+		}
+	}
 
-    public void calcularImc() {
-    	
-    	double imc = peso / altura * altura;
+	public double getAltura() {
+		return altura;
+	}
 
-    }
+	public void setPeso(double peso) {
 
-    public void classificarImc(){
+		if (peso > 0) {
+			// verdade
+			this.peso = peso;
+		} else {
+			// falso
+			System.out.println("O peso deve ser maior do que ZERO!");
+		}
 
-    }
+	}
 
-    public void exibirDados() {
-    	String unidadePeso = "Kg.";
-    	String unidadeAltura = "m.";
-    	System.out.println("-----------------------------------");
-    	System.out.println("dados do paciente".toUpperCase());
-    	System.out.println("-----------------------------------");
-    	System.out.println("nome: ".toUpperCase() + nome);
-    	System.out.printf("PESO: %s %s\n", peso, unidadePeso);
-    	System.out.printf("ALTURA: %s %s\n", altura, unidadeAltura);
-    	System.out.println("===================================");
-    	System.out.println();
-    }
+	public double getPeso() {
+		return this.peso;
+	}
+
+	public void marcarConsulta() {
+
+	}
+
+	public void calcularIdade() {
+
+	}
+
+	public void calcularImc() {
+
+		double imc = peso / altura * altura;
+
+	}
+
+	public void classificarImc() {
+
+	}
+
+	public void exibirDados() {
+		String unidadePeso = "Kg.";
+		String unidadeAltura = "m.";
+		System.out.println("-----------------------------------");
+		System.out.println("dados do paciente".toUpperCase());
+		System.out.println("-----------------------------------");
+		System.out.println("nome: ".toUpperCase() + nome);
+		System.out.printf("PESO: %s %s\n", peso, unidadePeso);
+		System.out.printf("ALTURA: %s %s\n", altura, unidadeAltura);
+		System.out.println("===================================");
+		System.out.println();
+	}
 
 }
